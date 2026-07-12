58dc9baa31e43f1bf0d895cd3ab8467c632d4b2e
Shamitsu212 <darklin314@gmail.com>
2026-07-12 21:27:28 +0300

    feat: Добавлена адаптация на телефон

--- ankiCards/src/pages/MainPage.tsx
+++ ankiCards/src/pages/MainPage.tsx
@@ -3,14 +3,27 @@ import styles from './MainPage.module.css'
 import Aside from '../components/Aside/Aside'
 import Content from '../components/Content/Content'
 
+import { useState } from 'react';
+
 function MainPage() {
+
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className={styles.page}>
 
-      <Aside/>
-      <Content/>
+      <button className={styles.burger} onClick={() => setIsOpen(!isOpen)}>
+        ☰
+      </button>
 
+      <div className={`${styles.aside} ${isOpen ? styles.open : ""}`}>
+        <Aside />
+      </div>
+
+      <div className={styles.content}>
+        <Content />
+      </div>
+      
     </div>
   )
 }
